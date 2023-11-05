d364f4100bf0124915a9b014c7dbcd4c8f4bd9ad
tsunamiprogram <131740677+tsunamiprogram@users.noreply.github.com>
2023-11-05 18:40:49 -0500

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,73 @@
+const bgByType = {
+        "normal": "bg-gradient-to-t from-yellow-100 to-yellow-300",
+        "fighting": "bg-gradient-to-t from-orange-700 to-orange-900",
+        "flying": "bg-gradient-to-t from-cyan-100 to-cyan-300",
+        "poison": "bg-gradient-to-t from-purple-500 to-purple-900",
+        "ground": "bg-gradient-to-t from-yellow-700 to-yellow-900",
+        "rock": "bg-gradient-to-t from-zinc-400 to-zinc-500",
+        "bug": "bg-gradient-to-t from-green-400 to-green-600",
+        "ghost": "bg-gradient-to-t from-purple-800 to-purple-950",
+        "steel": "bg-gradient-to-t from-gray-300 to-gray-500",
+        "fire": "bg-gradient-to-t from-orange-400 to-orange-600",
+        "water": "bg-gradient-to-t from-sky-400 to-sky-600",
+        "grass": "bg-gradient-to-t from-green-400 to-teal-400",
+        "electric": "bg-gradient-to-t from-indigo-500 to-indigo-950",
+        "psychic": "bg-gradient-to-t from-fuchsia-400 to-fuchsia-600",
+        "ice": "bg-gradient-to-t from-cyan-300 to-cyan-500",
+        "dragon": "bg-gradient-to-t from-cyan-500 to-cyan-700",
+        "dark": "bg-gradient-to-t from-gray-600 to-gray-800",
+        "fairy": "bg-gradient-to-t from-pink-400 to-pink-600",
+        "unknown": "bg-gradient-to-t from-yellow-300 to-yellow-500",
+        "shadow": "bg-gradient-to-t from-slate-800 to-slate-950",
+    
+};
+
+const borderByType = {
+    "normal": " border-yellow-300",
+    "fighting": "border-orange-900",
+    "flying": "border-cyan-300",
+    "poison": "border-purple-900",
+    "ground": "border-yellow-900",
+    "rock": "border-gray-500",
+    "bug": "border-green-500",
+    "ghost": " border-purple-950",
+    "steel": "border-gray-500",
+    "fire": " border-orange-500",
+    "water": "border-sky-600",
+    "grass": "border-green-300",
+    "electric": "border-indigo-800",
+    "psychic": "border border-fuchsia-600",
+    "ice": "border-cyan-500",
+    "dragon": "border-cyan-700",
+    "dark": "border-gray-800",
+    "fairy": "border-pink-600",
+    "unknown": " border-yellow-500",
+    "shadow": "border-slate-950",
+
+};
+const colorTxtByType = {
+    "normal": "text-yellow-100",
+    "fighting": "text-orange-700 ",
+    "flying": "text-cyan-100 ",
+    "poison": "text-blue-500 ",
+    "ground": "text-amber-300 ",
+    "rock": "text-zinc-500 ",
+    "bug": "text-green-500 ",
+    "ghost": "text-purple-800 ",
+    "steel": "text-gray-300 ",
+    "fire": "text-orange-400",
+    "water": "text-sky-400",
+    "grass": "text-green-300 ",
+    "electric": "text-blue-500 ",
+    "psychic": "text-fuchsia-400 ",
+    "ice": "text-cyan-300 ",
+    "dragon": "text-cyan-500 ",
+    "dark": "text-gray-600 ",
+    "fairy": "text-pink-400 ",
+    "unknown": "text-yellow-300 ",
+    "shadow": "text-slate-800 ",
+
+};
+export {
+    bgByType, borderByType, colorTxtByType
+};
