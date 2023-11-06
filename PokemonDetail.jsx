91e65e6c9eb942484d11c05ccb40b2f2536c8cb5
tsunamiprogram <131740677+tsunamiprogram@users.noreply.github.com>
2023-11-05 19:18:27 -0500

    Add files via upload

--- PokemonDetail.jsx
+++ PokemonDetail.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import HeaderPokeball from "../components/layouts/HeaderPokeball";
-import { bgByType, borderByType } from "../constants/pokemon";
+import { bgByType } from "../constants/pokemon";
 
 const PokemonDetail = () => {
   const [pokemon, setPokemon] = useState(null);
@@ -11,7 +11,7 @@ const PokemonDetail = () => {
   const firstType = pokemon?.types[0].type.name;
 
   const getPercentStats = (statsValue) => {
-    const MaxStatValue = 255;
+    const MaxStatValue = 150;
     const PercentStat = ((statsValue * 100) / MaxStatValue).toFixed(1);
     return `${PercentStat}%`;
   };
@@ -26,7 +26,7 @@ const PokemonDetail = () => {
   return (
     <main className=" text-center capitalize">
       <HeaderPokeball />
-      <article className={`max-w-[500px] mt-[185px] mx-auto capitalize border-4 p-1 rounded-lg border-slate-100 text-center`}
+      <article className={`max-w-[500px] mt-[100px] mx-auto capitalize border-4 p-1 rounded-lg border-slate-100 text-center`}
       >
         <header className={`relative ${bgByType[firstType]} h-[130px]`}><div className="absolute w-full top-0 -translate-y-1/2">
             <img
@@ -36,25 +36,28 @@ const PokemonDetail = () => {
           </div>
         </header>
         <div className="pt-6">
+          <div className="mx-auto justify-center ">
+            <h3  className=" max-w-[38px] text-xl text-gray-600 font-semibold border-2 mx-auto">#{pokemon?.id}</h3>
+          </div>
           
-          <h3  className="text-xl font-semibold">#{pokemon?.id}</h3>
-          <h2 className="text-2xl font-semibold">{pokemon?.name} </h2>
+          <h2 className="text-2xl font-semibold text-gray-600">{pokemon?.name} </h2>
           {/* stats */}
-          <section className="text-start">
+
+          <section className="text-start px-12 ">
             <h3>Stats</h3>
             <ul className="grid gap-4">
               {pokemon?.stats.map((stat) => (
                 <li className="capitalize" key={stat.stat.name}>
                   <div className="flex justify-between items-center">
                     <h5>{stat.stat.name}</h5>
-                    <span>{stat.base_stat}/255</span>
+                    <span>{stat.base_stat}/150</span>
                   </div>
                   {/* total Bar */}
                   <div className="bg-slate-200 rounded-md h-6">
                     {/* Bar Progress */}
                     <div
                       style={{ width: getPercentStats(stat.base_stat) }}
-                      className={"bg-yellow-400 h-full"}
+                      className={"bg-gradient-to-r from-amber-400 to-amber-700 h-full"}
                     ></div>
                   </div>
                 </li>
